cb122577e2ec51489c0745e782a9a0819f9b223a
Aimé T Shangula <31923592+aimazin@users.noreply.github.com>
2026-02-13 15:00:42 +0000

    Add files via upload

--- api_deploy.py
+++ api_deploy.py
@@ -37,4 +37,4 @@ async def heal_manifold(input_data: LiquidityData):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8008)
